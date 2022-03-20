9cfd35567e9226639b4c147a5eea5ca187c7a282
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-03-20 20:31:26 +0530

    chore: rename readme (#4)
    * chore: no need of todo
    
    * chore: readme rename

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,23 +1,22 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  clean: true,
-  dts: false,
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs', 'iife'],
-  minify: false,
-  skipNodeModulesBundle: true,
-  sourcemap: true,
-  target: 'es2021',
-  keepNames: true,
-  // TODO: change name
-  globalName: 'template',
-  tsconfig: 'src/tsconfig.json',
-  esbuildOptions: (options, context) => {
-    if (context.format === 'cjs') {
-      options.banner = {
-        js: '"use strict";',
-      };
-    }
-  },
+	clean: true,
+	dts: false,
+	entry: ['src/index.ts'],
+	format: ['esm', 'cjs', 'iife'],
+	minify: false,
+	skipNodeModulesBundle: true,
+	sourcemap: true,
+	target: 'es2021',
+	keepNames: true,
+	globalName: 'template',
+	tsconfig: 'src/tsconfig.json',
+	esbuildOptions: (options, context) => {
+		if (context.format === 'cjs') {
+			options.banner = {
+				js: '"use strict";',
+			};
+		}
+	},
 });
